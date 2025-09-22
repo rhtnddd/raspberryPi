3cfa05c8b18bed9445743703fe63113ccdb88868
rhtnddd <qlalfql123@gmail.com>
2025-09-22 17:34:50 +0900

    깜빡

---
+++ ch03/static/index.js
@@ -0,0 +1,11 @@
+let click = document.getElementById("click");
+let ggugim = document.getElementById("ggugim");
+let imageElement = document.getElementById("myImage");
+click.addEventListener('click', () => {
+    imageElement.src="../static/jungukugim.png";
+    imageElement.alt = "두 번째 이미지";
+});
+ggugim.addEventListener('click', () => {
+    imageElement.src="../static/junguggugim.png";
+    imageElement.alt = "첫 번째 이미지";
+});
