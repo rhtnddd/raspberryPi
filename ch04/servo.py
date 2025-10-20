26b0f78965487adc05bda09bf6642fd080199c0f
rhtnddd <qlalfql123@gmail.com>
2025-10-20 17:14:26 +0900

    ch04

---
+++ ch04/servo.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+import servo
+
+pin = 16
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pin, GPIO.OUT)
+
+def setAngle(angle):
+    duty = 2.5 + 10 * angle / 180
+    print(f"degree : {angle} to {duty}(duty)")
+    servo.ChangeDutyCycle(duty)
+
+if __name__ == "__main__":
+    servo.start(0)
+
+    setAngle(0)
+    sleep(1)
+    setAngle(90)
+    sleep(1)
+    setAngle(50)
+    sleep(1)
+    setAngle(120)
+    sleep(1)
+    setAngle(180)
+    sleep(1)
+
+	servo.stop()
+	GPIO.cleanup()
